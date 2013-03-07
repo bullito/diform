385859bc786c5073da574c1d479bec09600cef76
b.le <b.le@synapse-p2.synapse.local>
2013-03-07 12:07:09 +0100

    validator check

--- server/diform/validator.php
+++ server/diform/validator.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace diform;
+
 /**
  * Description of validator
  *
@@ -7,22 +9,24 @@ namespace diform;
  */
 class validator
 {
-    protected static $rules    =   array();
+
+    protected static $rules = array();
     protected static $feedback_default;
+    protected static $lang = 'en';
 
     public static function rule($matcher, $check, $feedback = null)
     {
         assert(is_string($matcher));
         assert(is_callable($check));
         assert(isset($feedback) || is_array($feedback));
-        
-        static::$rules[$matcher]   =   compact('name', 'check', 'feedback');        
+
+        static::$rules[$matcher] = compact('name', 'check', 'feedback');
     }
-    
+
     public static function is_disabled($control)
     {
         return isset($control->attributes['disabled']) &&
-        $control->attributes['disabled'];
+            $control->attributes['disabled'];
     }
 
     public static function rules4Attribute($attribute)
@@ -38,59 +42,112 @@ class validator
         {
             return true;
         }
-        
-        $o  =   array();
-        
-        foreach(static::$rules as $rule => $func)
+
+        $o = array();
+
+        foreach ($control->rules() as $matcher => $rule)
         {
-            $o[$rule]  =   !$func($control);
-        }
-        
-        foreach($control->rules() as $rule => $func)
-        {
-            $o['custom-'.$rule]  =  !$func($control);
+            ($rule === true) and ($rule = self::$rules[$matcher]);
+
+            if (is_array($rule) && is_callable($rule['check']))
+            {
+                ($rule['check']($control)) or 
+                    ($o[] = static::feedback4ControlAndRule($control, $rule))
+                ;
+                
+            }
         }
 
-        return array_filter($o) ?: true;
+        return array_filter($o) ? : true;
     }
+
+    public static function feedback4ControlAndRule($control, $rule)
+    {
+        if (($feedback = $control->attr('data-message')))
+        {
+            return $feedback;
+        }
+        
+        if (isset($rule['feedback']))
+        {
+            if (is_array($rule['feedback']))
+            {
+                $lang   =   static::lang4Control($control);
+                
+                if (isset($rule['feedback'][$lang]))
+                {
+                    return $rule['feedback'][$lang];
+                }
+                else if (($feedback = array_shift($rule['feedback'])))
+                {
+                    return $feedback;
+                }
+            }
+            else if (is_string($rule['feedback']))
+            {
+                return $rule['feedback'];
+            }
+        }
+        
+        return self::$feedback_default ?: 'invalid';
+    }
+    
+    public static function lang4Control($control)
+    {
+        if (isset($control->lang))
+        {
+            return $control->lang;
+        }
+        
+        if (isset($control->form->lang))
+        {
+            return $control->form->lang;
+        }
+        
+        return self::$lang;
+    }
+    
 }
 
-validator::rule('[required]', function($control) {
-    
-    return isset($control->val()) && !empty($control->val());
-}, array(
+validator::rule('[required]', function($control)
+    {
+
+        return isset($control->val()) && !empty($control->val());
+    }, array(
     'en' => 'required',
     'fr' => 'manquant'
 ));
 
-validator::rule('[pattern]', function($control) {
-    
-  return preg_match('~'.$control->attributes['pattern'].'~', $control->val());
-}, array(
-    'en'    =>  'wrong format',
-    'fr'    =>  'mal formaté'
+validator::rule('[pattern]', function($control)
+    {
+
+        return preg_match('~' . $control->attributes['pattern'] . '~', $control->val());
+    }, array(
+    'en' => 'wrong format',
+    'fr' => 'mal formaté'
 ));
 
 
-validator::rule('[minlength]', function($control) {
+validator::rule('[minlength]', function($control)
+    {
 
-    $minlength =   parseInt($control->attributes['minlength']);
-    $length    =   strlen($control->val());
-    
-    return ($length === 0 || $length >= $minlength);
-},
- array(
+        $minlength = parseInt($control->attributes['minlength']);
+        $length = strlen($control->val());
+
+        return ($length === 0 || $length >= $minlength);
+    }, array(
     'en' => 'too short',
     'fr' => 'trop court'
 ));
 
-validator::rule('[maxlength]', function($control) {
-    
-    $maxlength =   parseInt($control->attributes['minlength']);
-    $length    =   strlen($control->val());
-    
-    return ($length === 0 || $length >= $maxlength);
-}, array(
+validator::rule('[maxlength]', function($control)
+    {
+
+        $maxlength = parseInt($control->attributes['minlength']);
+        $length = strlen($control->val());
+
+        return ($length === 0 || $length >= $maxlength);
+    }, array(
     'en' => 'too long',
     'fr' => 'trop long'
 ));
