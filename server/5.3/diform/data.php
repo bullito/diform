1f48d3c38a676cdce405315055308536dbfcedd6
b.le <b.le@synapse-p2.synapse.local>
2013-04-15 16:05:51 +0200

    _value for dump and debug

--- server/5.3/diform/data.php
+++ server/5.3/diform/data.php
@@ -34,7 +34,7 @@ class data extends extendable
         if ($data)
         {
             $this->_isPopulated =   true;
-            $this->_extend(json_decode(json_decode($data)));    //  object/array
+            $this->_extend(json_decode(json_encode($data)));    //  object/array
         }
         
         return $this;
