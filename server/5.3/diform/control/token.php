1221906cd5d877cfcd02cba31cc1dab28c9f1680
ble <ble@jolicloud.jolicloud-domain>
2013-04-18 20:09:58 +0200

    token

--- server/5.3/diform/control/token.php
+++ server/5.3/diform/control/token.php
@@ -7,6 +7,9 @@ namespace diform\control;
  */
 class token extends \diform\control
 {
+    protected static $register_key = 'diform_tokens';
+    protected $generated;
+    
     public $attributes = array(
         'type' => 'hidden',
         'pattern' => '^-?\d*\.\d*$'
@@ -16,22 +19,60 @@ class token extends \diform\control
     {
         parent::__construct($form);
         
-        $token = static::register();
+        session_start();
+        $this->generated = static::register();
+        
         $this->rule('token', function($control) {
+            
             $class = get_class($control);
-            return in_array($control->val(), $class::registered());
+            
+            if (in_array($token = $control->val(), $class::registered()))
+            {
+                static::unregister($token);
+                return true;
+            }
         });
         
         $this->val();
     }
     
+    public static function generate()
+    {
+        return uniqid('diform_token_', true);
+    }
+
     public static function register() 
     {
-        $token = uniqid('diform_token_', true);
+        $token = static::generate();
+        
+        $_SESSION[static::$register_key][$token]  =   true;
+        
+        return $token;
     }
     
+    public static function unregister($token)
+    {
+        unset($_SESSION[static::$register_key][$token]);
+    }
+    
+    /**
+     * 
+     * @return array
+     */
     public function registered()
     {
-        return $_SESSION['diform_token'];
+        return $_SESSION[static::$register_key];
+    }
+    
+    /**
+     * avoid replacing value by request input
+     * request input will be kept 
+     * to compare with registered tokens
+     * @return \diform\control\token
+     */
+    public function populate()
+    {
+        $this->val();
+        return $this;
     }
 }
