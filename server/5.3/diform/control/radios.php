e492d7032860808d41988fcf8c920968b3c73138
ble <ble@jolicloud.jolicloud-domain>
2013-06-26 14:39:28 +0200

    remove val method

--- server/5.3/diform/control/radios.php
+++ server/5.3/diform/control/radios.php
@@ -10,13 +10,6 @@ class radios extends withchoices {
     protected $tag;
     protected $sub_control = 'radio';
     protected $labelization = array('type' => 'in', 'value' => 'right');
-
-    public function val()
-    {
-        return isset($this->form->data->{$this->name}) ?
-            $this->form->data->{$this->name} : null
-        ;
-    }
     
     public function items()
     {
