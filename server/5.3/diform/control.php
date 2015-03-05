850c324e9778a01b02c245b3cc902099859d1492
ble <buulam.le@gmail.com>
2015-03-05 17:13:52 +0100

    format

--- server/5.3/diform/control.php
+++ server/5.3/diform/control.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace diform;
 
 /**
@@ -10,57 +11,58 @@ namespace diform;
  * @property string $attrs html string of attributes/values
  *  
  */
-class control
-{
-    public static $args =   array('name', 'batch');
+class control {
+
+    public static $args = array('name', 'batch');
+
     /**
      * \diform instance whom control is attached to
      * @var \diform 
      */
     protected $form;
-    
+
     /**
      *
      * @var string 
      */
     protected $tag = 'input';
-    
+
     /**
      *
      * @var string 
      */
     protected $content;
-    
+
     /**
      *
      * @var mixed 
      */
     protected $val;
-    
+
     /**
      * label
      * @var string 
      */
     protected $label;
-    
+
     /**
      * tip
      * @var string 
      */
     protected $tip;
-    
+
     /**
      * feedback
      * @var string 
      */
     protected $feedback;
-    
+
     /**
      * 
      * @var string 
      */
     protected $name;
-    
+
     /**
      *
      * @var array 
@@ -72,47 +74,44 @@ class control
      * @var bool 
      */
     protected $checkValidity;
-    
+
     /**
      * assoc of html attributes of control
      * @var array 
      */
     protected $attributes = array();
-    
+
     /**
      *
      * @var synchronize val and attributes[value] 
      */
     protected $sync_value = true;
-    
+
     /**
      * 
      * @param \diform $form
      */
-    public function __construct($form = null)
-    {
+    public function __construct($form = null) {
         $this->form = $form;
-        
-        foreach($this->attributes as $attr => $value)
-        {
+
+        foreach ($this->attributes as $attr => $value) {
             $this->attr($attr, $value);
         }
     }
-    
 
     /**
      * Return method result or attribute
      * @param string $name
      * @return mixed
      */
-    public function __get($name)
-    {
-        if (isset($this->$name)) return $this->$name;
-        
-        return method_exists($this, $name) ? 
-            ($this->$name = $this->$name()) : 
+    public function __get($name) {
+        if (isset($this->$name))
+            return $this->$name;
+
+        return method_exists($this, $name) ?
+            ($this->$name = $this->$name()) :
             (isset($this->attributes[$name]) ?
-                $this->attributes[$name] : 
+                $this->attributes[$name] :
                 null
             )
         ;
@@ -123,87 +122,69 @@ class control
      * @param string $name
      * @return mixed
      */
-    public function __set($name, $value)
-    {
-        return method_exists($this, $name) ? 
-            $this->$name($value) : 
+    public function __set($name, $value) {
+        return method_exists($this, $name) ?
+            $this->$name($value) :
             $this->attributes[$name] = $value
         ;
     }
-    
+
     /**
      * 
      * @param string $name
      * @param array $args
      * @return \diform\control
      */
-    public function __call($name, $args)
-    {
+    public function __call($name, $args) {
         unset($this->attrs);
 
-        if (!count($args))
-        {
+        if (!count($args)) {
             $this->attr($name, true);
-        }
-        else if ($name == 'class')
-        {
+        } else if ($name == 'class') {
             $this->attr($name, array_filter(array_unique(
-                explode(' ', $args[0])
+                        explode(' ', $args[0])
             )));
-        }
-        else
-        {
+        } else {
             $this->attr($name, $args[0]);
         }
 
         return $this;
     }
 
-    public function attr($name /* , $value = null */)
-    {
-        if (preg_match('/[^a-zA-Z0-9_-]/', $name))
-        {
-            throw new exception(__METHOD__.": attribute name '$name' - wrong format");
+    public function attr($name /* , $value = null */) {
+        if (preg_match('/[^a-zA-Z0-9_-]/', $name)) {
+            throw new exception(__METHOD__ . ": attribute name '$name' - wrong format");
         }
-        
-        if (func_num_args() == 2)
-        {
+
+        if (func_num_args() == 2) {
             unset($this->attrs);
-            $value  =   func_get_arg(1);
-            
+            $value = func_get_arg(1);
+
             $this->attributes[$name] = $value;
-            
-            switch($name)
-            {
-            case 'name':
-                $this->name = preg_match('/^(.+)\[\]$/', $value, $matches) ?
-                    $matches[1] : $value
-                ;
-                break;
+
+            switch ($name) {
+                case 'name':
+                    $this->name = preg_match('/^(.+)\[\]$/', $value, $matches) ?
+                        $matches[1] : $value
+                    ;
+                    break;
             }
-            
-            if ($value === false)
-            {
+
+            if ($value === false) {
                 unset($this->rules["[$name]"]);
+            } else if (($rule = validator::rules4Attribute($name))) {
+                $this->rules[$rule['matcher']] = true;
             }
-            else if (($rule = validator::rules4Attribute($name)))
-            {
-                $this->rules[$rule['matcher']]  =   true;
-            }
-            
+
             return $this;
-        }
-        else
-        {
+        } else {
             return isset($this->attributes[$name]) ?
                 $this->attributes[$name] : null;
         }
     }
 
-    public function has_multiple()
-    {
-        return $this->has_multiple =   isset($this->attributes['multiple']) 
-            && $this->attributes['multiple']
+    public function has_multiple() {
+        return $this->has_multiple = isset($this->attributes['multiple']) && $this->attributes['multiple']
         ;
     }
 
@@ -211,8 +192,7 @@ class control
      * 
      * @return string
      */
-    public function attrs()
-    {
+    public function attrs() {
         $this->populate();
         $this->checkValidity();
         return $this->attrs = \diform::attrs($this->attributes);
@@ -223,99 +203,73 @@ class control
      * @param string $value
      * @return string|\diform\control
      */
-    public function label($value = null)
-    {
+    public function label($value = null) {
         return (func_num_args() && (($this->{__FUNCTION__} = $value) || true)) ?
             $this : $this->{__FUNCTION__}
         ;
     }
-    
+
     /**
      * setter/getter tip
      * @param string $value
      * @return string|\diform\control
      */
-    public function tip($value = null)
-    {
+    public function tip($value = null) {
         return (func_num_args() && (($this->{__FUNCTION__} = $value) || true)) ?
             $this : $this->{__FUNCTION__}
         ;
     }
-    
-    public function val($value = null)
-    {
-        if (func_num_args())
-        {
+
+    public function val($value = null) {
+        if (func_num_args()) {
             $this->val = $value;
             return $this;
-        }
-        else if (isset($this->val))
-        {
+        } else if (isset($this->val)) {
             return $this->val;
-        }
-        else if (isset($this->name) && ($this->form->data->isPopulated() || true) && isset($this->form->data->{$this->name}))
-        {
+        } else if (isset($this->name) && ($this->form->data->isPopulated() || true) && isset($this->form->data->{$this->name})) {
             return $this->val = $this->form->data->{$this->name};
-        }
-        else
-        {
+        } else {
             return null;
         }
     }
 
-    public function populate()
-    {
+    public function populate() {
         $this->val();
-        
-        if ($this->sync_value && isset($this->val))
-        {
+        if ($this->sync_value && isset($this->val)) {
             $this->attributes['value'] = $this->val;
         }
-
         return $this;
     }
 
-    public function prepare()
-    {
+    public function prepare() {
         return $this;
     }
 
-    public function render($return = false)
-    {
+    public function render($return = false) {
         $this->attrs();
         $this->form and $this->form->events->trigger('render_control', $this);
         $this->prepare();
-        
+
         $return && ob_start();
         $has_tag = isset($this->tag);
         $has_content = isset($this->content);
-        
-        if ($has_tag)
-        {
+
+        if ($has_tag) {
             echo '<', $this->tag;
-            if ($this->attrs)
-            {
+            if ($this->attrs) {
                 echo ' ', $this->attrs;
             }
-            if ($has_content)
-            {
+            if ($has_content) {
                 echo '>';
             }
         }
-
-        if ($has_content)
-        {
+        if ($has_content) {
             echo $this->content;
         }
-
-        if ($has_tag)
-        {
-            if ($has_content)
-            {
+        if ($has_tag) {
+            if ($has_content) {
                 echo '</', $this->tag, '>';
-            }
-            else
-            {
+            } else {
                 echo ' />';
             }
         }
@@ -323,57 +277,49 @@ class control
         return $return ? ob_get_clean() : $this;
     }
 
-    public function __toString()
-    {
+    public function __toString() {
         return $this->render(true);
     }
 
-    public function checkValidity()
-    {
-        if (isset($this->checkValidity))
+    public function checkValidity() {
+        if (isset($this->checkValidity)) {
             return $this->checkValidity;
-        
-        if ($this->form && $this->form->data->isPopulated())
-        {
-            $check  =   validator::checkValidity($this);
-        
-            if ($check !== true)
-            {
+        }
+        if ($this->form && $this->form->data->isPopulated()) {
+            $check = validator::checkValidity($this);
+
+            if ($check !== true) {
                 $this->invalidate($check);
             }
-            
             return ($this->checkValidity = $check);
         }
-        
         return true;
     }
-    
+
     /**
      * @todo externalize error template
      * @param string $error
      * @return \diform\control
      */
-    public function error($error)
-    {
+    public function error($error) {
         static $error_decorator = false;
         $error_decorator or $error_decorator = $this->form->config->error_decorator;
-        
+
         $this->feedback(str_replace('${error}', $error, $error_decorator));
         return $this;
     }
-    
+
     /**
      * 
      * @param array $value
      * @return array|\diform\control
      */
-    public function rules($value = null)
-    {
+    public function rules($value = null) {
         return (func_num_args() && (($this->{__FUNCTION__} = $value) || true)) ?
             $this : $this->{__FUNCTION__}
         ;
     }
-    
+
     /**
      * 
      * @param string $name
@@ -381,43 +327,34 @@ class control
      * @param array $feedback
      * @return \diform\control
      */
-    public function rule($name, $check, $feedback = null)
-    {
+    public function rule($name, $check, $feedback = null) {
         assert(!empty($name));
         assert(is_callable($check) or function_exists($check));
         unset($this->checkValidity);
-        $this->rules[$name] =   compact('name', 'check', 'feedback');
+        $this->rules[$name] = compact('name', 'check', 'feedback');
         return $this;
     }
-    
-    public function addClass($class)
-    {
-        if (!isset($this->attributes['class']))
-        {
+
+    public function addClass($class) {
+        if (!isset($this->attributes['class'])) {
             $this->attributes['class'][] = $class;
         }
-        if (is_string($this->attributes['class']))
-        {
+        if (is_string($this->attributes['class'])) {
             $this->attributes['class'] = (array) $this->attributes['class'];
         }
-        if (!in_array($class, $this->attributes['class']))
-        {
+        if (!in_array($class, $this->attributes['class'])) {
             $this->attributes['class'][] = $class;
         }
         return $this;
     }
 
-    public function removeClass($class)
-    {
-        if (!is_array($this->attributes['class']))
-        {
+    public function removeClass($class) {
+        if (!is_array($this->attributes['class'])) {
             $this->attributes['class'] = (array) $this->attributes['class'];
         }
 
-        foreach ($this->attributes['class'] as $rnk => $nclass)
-        {
-            if ($nclass == $class)
-            {
+        foreach ($this->attributes['class'] as $rnk => $nclass) {
+            if ($nclass == $class) {
                 unset($this->attributes['class'][$rnk]);
             }
         }
@@ -430,8 +367,7 @@ class control
      * @param \diform $value
      * @return \diform\control|\diform
      */
-    public function form(\diform $value = null)
-    {
+    public function form(\diform $value = null) {
         return (func_num_args() && (($this->{__FUNCTION__} = $value) || true)) ?
             $this : $this->{__FUNCTION__}
         ;
@@ -442,8 +378,7 @@ class control
      * @param array $value
      * @return array|\diform\control
      */
-    public function attributes($value = null)
-    {
+    public function attributes($value = null) {
         return (func_num_args() && (($this->{__FUNCTION__} = $value) || true)) ?
             $this : $this->{__FUNCTION__}
         ;
@@ -453,39 +388,32 @@ class control
      * @param string $value
      * @return string|\diform\control
      */
-    public function feedback($value = null)
-    {
+    public function feedback($value = null) {
         if (func_num_args()) {
             $this->{__FUNCTION__} = $value;
-        }
-        else
-        {
+        } else {
             $this->checkValidity();
             return $this->{__FUNCTION__};
         }
     }
-    
-    public function invalidate($error = '')
-    {
+
+    public function invalidate($error = '') {
         $this->form->events->trigger('invalidate_control', $this);
         $this->error($error);
         return $this;
     }
-    
+
     /**
      * @param array|object|\Traversable $batch
      * @return \diform\control
      */
-    public function batch($batch)
-    {
-        if ($batch)
-        {
-            foreach($batch as $method => $arg)
-            {
+    public function batch($batch) {
+        if ($batch) {var_dump($batch);
+            foreach ($batch as $method => $arg) {
                 $this->$method($arg);
             }
         }
-        
         return $this;
     }
+
 }
