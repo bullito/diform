97b5b0f3f3154f2dc1fb295afbc4a941e1a0a06f
ble <buulam.le@gmail.com>
2015-03-05 17:09:28 +0100

    format

--- server/5.3/diform/extendable.php
+++ server/5.3/diform/extendable.php
@@ -1,39 +1,38 @@
 <?php
+
 namespace diform;
+
 /**
  * Description of extendable
  *
  * @author b.le
  */
-class extendable
-{
+class extendable {
+
     /**
      *
      * @var \diform
      */
     protected $_diform;
-    
+
     /**
      *
      * @var array 
      */
     public static $defaults = array();
 
-    public function __construct(\diform $diform = null, $extend = null)
-    {
-        $this->_diform  =   $diform;
+    public function __construct(\diform $diform = null, $extend = null) {
+        $this->_diform = $diform;
         $this->extend(static::$defaults)->extend($extend);
     }
-    
-    public function extend($data)
-    {
-        if (isset($data))
-        {
-            foreach ($data as $key => $val) 
+
+    public function extend($data) {
+        if (isset($data)) {
+            foreach ($data as $key => $val) {
                 $this->$key = $val;
+            }
         }
-        
         return $this;
     }
-}
 
+}
