f5ced661a780bb7561afc2c3c99cbdea554beb3c
ble <buulam.le@gmail.com>
2015-03-05 17:13:12 +0100

    format

--- server/5.3/diform/events.php
+++ server/5.3/diform/events.php
@@ -7,36 +7,34 @@ namespace diform;
  *
  * @author b.le
  */
-class events extends extendable
-{
+class events extends extendable {
+
     protected $render_form = array();
     protected $render_control = array();
     protected $invalidate_form = array();
     protected $invalidate_control = array();
-    
-    public function __construct(\diform $diform, $events = null)
-    {
+
+    public function __construct(\diform $diform, $events = array()) {
         $this->_diform = $diform;
-        
-        if ($events)
-            foreach($events as $event => $callbacks)
-                if (isset($this->$event))
-                    $this->$event =   $callbacks;
+
+        foreach ($events as $event => $callbacks) {
+            if (isset($this->$event)) {
+                $this->$event = $callbacks;
+            }
+        }         
     }
-    
-    public function on($event, $callback)
-    {
-        if (is_array($this->$event))
-            $this->{$event}[] =   $callback;
-        
+
+    public function on($event, $callback) {
+        if (is_array($this->$event)) {
+            $this->{$event}[] = $callback;
+        }
         return $this;
     }
-    
-    public function trigger($event, $instance)
-    {
-        foreach($this->$event as $callback)
+
+    public function trigger($event, $instance) {
+        foreach ($this->$event as $callback) {
             $callback($instance);
-        
+        }
         return $this;
     }
 }
