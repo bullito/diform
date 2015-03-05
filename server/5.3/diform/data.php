654a72199c07b350f83bcb243da90640fc7a261c
ble <buulam.le@gmail.com>
2015-03-05 17:13:45 +0100

    format

--- server/5.3/diform/data.php
+++ server/5.3/diform/data.php
@@ -7,151 +7,127 @@ namespace diform;
  *
  * @author ble
  */
-class data extends extendable
-{
+class data extends extendable {
 
     public static $defaults = array();
     public static $classmap = array();
+
     /**
      *
      * @var boolean 
      */
     protected $_isPopulated;
-    
+
     /**
      *
      * @var array 
      */
     protected $_raw = array();
+
     /**
      *
      * @var array 
      */
     protected $_value;
-    
+
     /**
      * 
      * @param array|object $data
      * @return \diform\data
      */
-    public function extend($data)
-    {
-        $this->_extend($data);    //  object/array
+    public function extend($data) {
+        $this->_extend($data);
         return $this;
     }
 
     /**
      * 
-     * @param array $data
+     * @param array|object|\Traversable $data
      * @param string $prefix
      */
-    protected function _extend($data, $prefix = '')
-    {
-        if ($data)
-        {
-            foreach ($data as $key => $value)
-            {
+    protected function _extend($data, $prefix = '') {
+        if ($data) {
+            foreach ($data as $key => $value) {
                 $name = $prefix ? ($prefix . '[' . $key . ']') : $key;
 
-                if (is_object($value))
-                {
-                    $class  =   get_class($value);
-                    
-                    if (isset(static::$classmap[$class]))
-                    {
-                        $map    =   static::$classmap[$class];
-                        $value  =   $map($value);
+                if (is_object($value)) {
+                    $class = get_class($value);
+
+                    if (isset(static::$classmap[$class])) {
+                        $map = static::$classmap[$class];
+                        $value = $map($value);
                     }
-                    
-                    if (is_object($value))
-                    {
+
+                    if (is_object($value)) {
+                        $this->_extend($value, $name);
+                        continue;
+                    }
+                } else if (is_array($value)) {
+                    if (is_string(key($value))) {
+                        $this->_extend($value, $name);
+                        continue;
+                    } else if (!is_scalar(current($value))) {
                         $this->_extend($value, $name);
                         continue;
                     }
                 }
-                else if (is_array($value))
-                {
-                    if (is_string(key($value)))
-                    {
-                        $this->_extend($value, $name);
-                        continue;
-                    }
-                    else if (!is_scalar(current($value)))
-                    {
-                        $this->_extend($value, $name);
-                        continue;
-                    }
-                }
-                
-                $this->$name = $this->_raw[$name] = $value; 
+
+                $this->$name = $this->_raw[$name] = $value;
             }
         }
-        
     }
 
     /**
      * 
      * @return boolean
      */
-    public function isPopulated()
-    {
-        if (!isset($this->_isPopulated))
-        {
-            $this->_isPopulated =   false;
-            $this->request($GLOBALS['_'.strtoupper($this->_diform->config->form['method'])]);
+    public function isPopulated() {
+        if (!isset($this->_isPopulated)) {
+            $this->_isPopulated = false;
+            $this->request($GLOBALS['_' . strtoupper($this->_diform->config->form['method'])]);
         }
-        
+
         return $this->_isPopulated;
     }
-    
+
     /**
      * 
      * @return assoc
      */
-    public function value()
-    {
-        if (!isset($this->_value))
-        {
+    public function value() {
+        if (!isset($this->_value)) {
             $this->_value = array();
-            
-            foreach($this->_diform->controls() as $key => $control)
-            {
-                if ($control->checkValidity() === true && isset($this->$key))
-                { 
-                    $nodes  =   preg_split('~\]?\[~', trim($key,']'));
-                    //var_dump($nodes);
-                    $branch =   &$this->_value;
-                    $final  =   array_pop($nodes);
-                    foreach($nodes as $node)
-                    {
-                        isset($branch[$node]) or $branch[$node] =   array();
-                        $branch =   &$branch[$node];
+
+            foreach ($this->_diform->controls() as $key => $control) {
+                if ($control->checkValidity() === true && isset($this->$key)) {
+                    $nodes = preg_split('~\]?\[~', trim($key, ']'));
+                    $branch = &$this->_value;
+                    $final = array_pop($nodes);
+                    foreach ($nodes as $node) {
+                        isset($branch[$node]) or $branch[$node] = array();
+                        $branch = &$branch[$node];
                     }
-                    $branch[$final] =   $this->$key;   
+                    $branch[$final] = $this->$key;
                 }
             }
         }
         return $this->_value;
     }
-    
-    public function request($data)
-    {
-        if ($data)
-        {
-            $this->_isPopulated =   true;
-            $this->extend($data);    //  object/array
+
+    public function request($data) {
+        if ($data) {
+            $this->_isPopulated = true;
+            $this->extend($data);
         }
-        
+
         return $this;
     }
-    
+
     /**
      * 
      * @return assoc
      */
-    public function raw()
-    {
+    public function raw() {
         return $this->_raw;
     }
 }
-
